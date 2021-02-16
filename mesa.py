76bf31915892c8551635f5a87a43260886a6c984
Alderbane <51372365+Alderbane@users.noreply.github.com>
2021-02-16 08:44:45 -0600

    Create mesa.py

---
+++ mesa.py
@@ -0,0 +1,17 @@
+class Mesa():
+    def __init__(self):
+        self.total = 0.0
+        self.lista_platillos = []
+    def agregar_platillo(self, platillo):
+        self.lista_platillos.append(platillo)
+    def cancelar_platillo(self, platillo):
+        self.lista_platillos.remove(platillo)
+    def imprimir_platillos(self)->str:
+        a = ""
+        for x in self.lista_platillos:
+            a = a + x.imprimir_descripcion() + "\n"
+    def calcular_total(self)->float:
+        self.total= 0.0
+        for x in self.lista_platillos:
+            self.total += x.precio
+        return total
