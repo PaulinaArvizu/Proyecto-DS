5c853ddc39ef4088a3726c149a6fd20e2c2a0634
Anahi S <is715773@iteso.mx>
2021-02-16 08:47:50 -0600

    Changed menu item to interface, added platillo and bebida

--- restaurante.py
+++ restaurante.py
@@ -1,13 +1,43 @@
-class Menu_Item:
+from abc import ABC
+
+
+class Menu_Item(ABC):
     def __init__(self, nombre: str, precio: float, ingredientes: list, descripcion: str):
         self.nombre = nombre
         self.precio = precio
-        descripcion = descripcion
+        self.descripcion = descripcion
         self.ingredientes = ingredientes
-    
+
     def preparar(self) -> bool:
-        print(f"Preparando un delicioso {self.nombre}");
+        print(f"Preparando un delicioso {self.nombre}")
         return True
-    
+
     def imprimir_descripcion(self) -> str:
         return f'{self.nombre}: {self.descripcion}'
+
+
+class Platillo(Menu_Item):
+    def preparar(self) -> bool:
+        print(f"Preparando un delicioso {self.nombre}")
+        return True
+
+    def imprimir_descripcion(self) -> str:
+        return f'{self.nombre}: {self.descripcion}'
+
+
+class Bebida(Menu_Item):
+    def preparar(self) -> bool:
+        print(f"Sirviendo un delicioso {self.nombre}")
+        return True
+
+    def imprimir_descripcion(self) -> str:
+        return f'{self.nombre}: {self.descripcion}'
+
+
+cocacola = Bebida('Coca-cola', 15.00,
+                  "Refrescante bebida efervescente", "Azucar, agua. colorante...")
+print(cocacola.preparar())
+
+pizza_margarita = Platillo('Pizza margarita', 150.00,
+                           "Pizza margarita", "queso, salsa, albahacar")
+print(pizza_margarita.preparar())
