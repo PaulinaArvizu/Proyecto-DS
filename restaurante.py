2d0476662356ccbcd29636d293136199f020842f
Anahi S <is715773@iteso.mx>
2021-02-16 08:49:28 -0600

    added mesa

--- restaurante.py
+++ restaurante.py
@@ -16,6 +16,30 @@ class Menu_Item(ABC):
         return f'{self.nombre}: {self.descripcion}'
 
 
+class Mesa():
+    def __init__(self):
+        self.total = 0.0
+        self.lista_platillos = []
+
+    def agregar_platillo(self, platillo):
+        self.lista_platillos.append(platillo)
+
+    def cancelar_platillo(self, platillo):
+        self.lista_platillos.remove(platillo)
+
+    def imprimir_platillos(self) -> str:
+        a = ""
+        for x in self.lista_platillos:
+            a = a + x.imprimir_descripcion() + "\n"
+        return a
+
+    def calcular_total(self) -> float:
+        self.total = 0.0
+        for x in self.lista_platillos:
+            self.total += x.precio
+        return total
+
+
 class Platillo(Menu_Item):
     def preparar(self) -> bool:
         print(f"Preparando un delicioso {self.nombre}")
